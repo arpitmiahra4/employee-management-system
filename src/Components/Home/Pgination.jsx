2c71005d1f5aab79ca1f1277337450afa9a55a27
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-13 15:04:25 +0530

    updated: appcontext and added: api crud functions

---
+++ src/Components/Home/Pgination.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pgination = () => {
+  return (
+    <div>Pgination</div>
+  )
+}
+
+export default Pgination
--- src/api.js
+++ src/api.js
@@ -1,4 +1,56 @@
+const URL = "http://localhost:4000/employee"
 
-export const getData = ({FilterByGender,FilterBy,titleSortBy,page,setTotalProduct,limit}) =>{
-    
+export const GetData = ({FilterByGender,FilterBy,titleSortBy,page,setTotalProduct,limit}) =>{
+    if(FilterBy !== null){
+        return fetch(`${URL}?department=${FilterBy}&_sort=name&_order=${titleSortBy}&_page=${page}&_limit=${limit}`)
+        .then(res =>{
+            setTotalProduct(+(res.headers.get("X-Total-Count")));
+            return res.json();
+        });
+    };
+    if(FilterByGender !== null){
+        return fetch(`${URL}?gender=${FilterByGender}&_sort=name&_order=${titleSortBy}&_page=${page}&_limit=${limit}`)
+        .then(res =>{
+            setTotalProduct(+(res.headers.get("X-Total-Count")));
+            return res.json();
+        });
+    }
+
+    return fetch(`${URL}?_sort=name&_order=${titleSortBy}&_page=${page}&_limit=${limit}`)
+    .then(res =>{
+        setTotalProduct(+(res.headers.get("X-Total-Count")));
+        return res.json();
+    });
+};
+
+export const AddData = (userData) =>{
+    return fetch(`${URL}`,{
+        method:"POST",
+        headers:{
+            "Content-Type" : "application/json" 
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res => res.json());
+};
+
+export const DeleteData = (id) =>{
+    return fetch(`${URL}/${id}`,{
+        method:"DELETE",
+        headers:{
+            "Content-Type" : "application/json"
+        },
+    })
+    .then(res => res.json());
+};
+
+export const UpdateData = (id,newData) =>{
+    return fetch(`${URL}/${id}`,{
+        method:"PATCH",
+        headers:{
+            "Content-Type" : "application/json" 
+        },
+        body: JSON.stringify(newData)
+    })
+    .then(res => res.json());
 }
