fb80a110a93c954c106a4677bfc817578e6e2fd2
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-14 15:37:52 +0530

    adding: loader

--- src/Components/Home/SingleEmployeePage.jsx
+++ src/Components/Home/SingleEmployeePage.jsx
@@ -54,8 +54,8 @@ const SingleEmployeePage = () => {
 
   if (loading) {
     return (
-      <VStack marginTop="10%">
-        <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/04de2e31234507.564a1d23645bf.gif" />
+      <VStack>
+        <Image src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e0b072897469.5bf6e79950d23.gif" w={"100%"} h={"90vh"}/>
       </VStack>
     )
   }
