ef14328083ebed0e314a24166ad076d2445f8c9a
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-20 09:29:53 +0530

    adding

--- server/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-require('dotenv').config();
-mongoose.set('strictQuery', true);
-const connection = mongoose.connect(process.env.MONGO_URL);
-
-module.exports = { connection };
